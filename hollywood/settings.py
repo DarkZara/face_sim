95e22f2e59e6e4996543b473750055ee5693c4c3
Alireza Sedighi Fard <48380433+DarkZara@users.noreply.github.com>
2022-12-07 12:39:02 -0800

    Update settings.py

--- hollywood/settings.py
+++ hollywood/settings.py
@@ -132,7 +132,7 @@ CORS_ORIGIN_ALLOW_ALL=True
 REST_FRAMEWORK ={'DEFAULT_PERMISSION_CLASSES': [   'rest_framework.permissions.AllowAny' ]}
 
 
-CORS_ALLOWED_ORIGINS = [
-    '*',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     '*',
+# ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
